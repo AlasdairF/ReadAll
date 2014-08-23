2e30f0478b2691c92b5a1a6db0b6f199852e6a00
AlasdairF <AlasdairF@users.noreply.github.com>
2014-08-23 14:19:30 +0700

    added

--- readall.go
+++ readall.go
@@ -1,11 +1,14 @@
 package readall
 
+import "io"
+
+// Replaces ioutil.ReadAll, when you know the (uncompressed) length.
 func (r io.Reader) ReadAll(s int) ([]byte, error) {
 	var n,m int
 	var e error
 	b := make([]byte,s)
 	for l:=s; l>0; l-=m {
-		m, e = fz.Read(b[n:s])
+		m, e = r.Read(b[n:s])
 		n += m
 		if e == io.EOF {
 			return b[0:n], nil
